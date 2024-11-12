7f38c0103a4694dc4c1eec3595b1c3053d8430c1
valenpucha <valenpucha159@gmail.com>
2024-11-12 16:44:23 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function Zapatilla3() {
+    window.location.href="https://www.solodeportes.com.ar/zapatillas-adidas-racer-tr23-negra.html"
+}
+
+function Zapatilla2() {
+    window.location.href="https://www.solodeportes.com.ar/zapatillas-de-tenis-adidas-defiant-speed-verde.html"
+}
+
+function Zapatilla1() {
+    window.location.href="https://www.solodeportes.com.ar/zapatillas-adidas-grand-court-2-0-ni-o-ni-a-negra-89353130.html"
+}
